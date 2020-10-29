11d20e72048509a1461c3c15127784a839f987cd
Rajendra Shrestha <rajendrashr@gmail.com>
2020-10-29 11:12:13 -0700

    Added callback function to each case of choices of optionMenu function

--- server.js
+++ server.js
@@ -19,6 +19,44 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    // optionMenu();
+    optionMenu();
 });
 
+function optionMenu() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "Please choose what you would like to perform!",
+        choices: [
+            "Add department", "Add roles", "Add an employee", "View departments", "View roles", "View employees", "Update an employee roles", "Exit"
+        ]
+    }).then(function (answer) {
+        switch (answer.action) {
+            // Add a callback function to each actions
+            case "Add department":
+                addDepartment();
+                break;
+            case "Add roles":
+                addRoles();
+                break;
+            case "Add an employee":
+                addEmployee();
+                break;
+            case "View departments":
+                viewDepartment();
+                break;
+            case "View roles":
+                viewRoles();
+                break;
+            case "View employees":
+                viewEmployees();
+                break;
+            case "Update an employee roles":
+                updateEmployees();
+                break;
+            case "Exit":
+                console.log("Thanks for using this app!");
+                connection.end();
+        }
+    });
+};
